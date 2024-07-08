46ccba91f5f2844b4d6fd54288b1d7ff5d5beca0
Azbian <mdneon255@gmail.com>
2024-07-08 23:46:51 +0600

    Previous Ground_station added

---
+++ Ground_Station/SAT_position.py
@@ -0,0 +1,64 @@
+import ephem
+import urllib.request
+import time
+import serial
+import os
+
+
+
+# Latitude and longitude of he observer (in degrees)
+#BSMRAAU
+observer_lat = '23.7719'   
+observer_lon = '90.3892'
+
+#Dhaka
+#observer_lat = '23.7131986'  
+#observer_lon = '90.4016137'
+
+#False location
+#observer_lat = '347.143'  
+#observer_lon = '266.63'
+
+# User input and change that name according to URL
+sat_name= input("Enter the name of the CUBESAT (e.g. CUTE-1 (CO-55)): ")
+sat=sat_name
+i=sat.find(' ')
+if i>0:
+    sat=sat[:i]+"%20"+sat[i+1:]
+# URL for the TLE data
+url = f'https://celestrak.org/NORAD/elements/gp.php?NAME={sat}&FORMAT=tle'
+
+# Create an ephem observer object for the observer's location
+observer = ephem.Observer()
+observer.lat = ephem.degrees(observer_lat)
+observer.lon = ephem.degrees(observer_lon)
+
+# Fetch TLE data
+req = urllib.request.urlopen(url)
+TLEdata = req.read().decode("utf-8")
+lines = TLEdata.strip().split('\n')
+
+
+
+while True:
+    # Create an ephem sat object
+    sat = ephem.readtle(lines[0], lines[1], lines[2])
+
+    # Set the observer's date and time to the current time
+    observer.date = ephem.now()
+
+    # Compute the sat's position
+    sat.compute(observer)
+
+    # Get the azimuth and elevation angles and convert them radian to degree
+    azimuth = sat.az * 180 / ephem.pi
+    elevation = sat.alt * 180 / ephem.pi
+
+    # Print the angles
+    os.system('cls')
+    print("Real time : "+str(ephem.localtime(ephem.now())))
+    print("Estimated time : "+str(ephem.localtime(observer.next_pass(sat)[0])-ephem.localtime(ephem.now())))
+    print(f"Azimuth: {azimuth:.2f}°, Elevation: {elevation:.2f}°")
+    data = f'{azimuth:.4f},{elevation:.4f}'
+    # Send angles through 'COM' port 
+    time.sleep(0.5)
