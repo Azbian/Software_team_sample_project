397b4e2b86f8ffde21176103bd07e665b0975ec0
ehasnur14 <ers420bd@gmail.com>
2024-07-14 23:26:24 +0600

    New Code

--- Ground_Station/SAT_position.py
+++ Ground_Station/SAT_position.py
@@ -1,53 +1,35 @@
 import ephem
 import urllib.request
-import time
-import os
 
 class SatelliteTracker:
     def __init__(self, observer_latitude, observer_longitude, satellite_name):
+        """
+        Initialize the SatelliteTracker with the observer's latitude and longitude and the satellite's name.
+        """
         self.observer = ephem.Observer()
         self.observer.lat = ephem.degrees(observer_latitude)
         self.observer.lon = ephem.degrees(observer_longitude)
         self.sat_name = satellite_name
-        self.tle_url = self._create_tle_url(satellite_name)
-        self.tle_data = self._fetch_tle_data()
-        self.sat = self._create_satellite(self.tle_data)
+        self.tle_url = self.create_tle_url(satellite_name)
+        self.tle_data = self.fetch_tle_data()
 
-    def _create_tle_url(self, satellite_name):
+    def create_tle_url(self, satellite_name):
+        """
+        Create the URL to fetch TLE data for the given satellite name.
+        """
         sat = satellite_name.replace(' ', '%20')
         return f'https://celestrak.org/NORAD/elements/gp.php?NAME={sat}&FORMAT=tle'
 
-    def _fetch_tle_data(self):
+    def fetch_tle_data(self):
+        """
+        Fetch the TLE data from the URL and return it as a list of lines.
+        """
         req = urllib.request.urlopen(self.tle_url)
         tle_data = req.read().decode("utf-8").strip().split('\n')
         return tle_data
 
-    def _create_satellite(self, tle_data):
-        return ephem.readtle(tle_data[0], tle_data[1], tle_data[2])
-
-    def track(self):
-        while True:
-            self.observer.date = ephem.now()
-            self.sat.compute(self.observer)
-
-            azimuth = self.sat.az * 180 / ephem.pi
-            elevation = self.sat.alt * 180 / ephem.pi
-
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print(f"Real time: {ephem.localtime(ephem.now())}")
-            print(f"Estimated time to next pass: {ephem.localtime(self.observer.next_pass(self.sat)[0]) - ephem.localtime(ephem.now())}")
-            print(f"Azimuth: {azimuth:.2f}°, Elevation: {elevation:.2f}°")
-
-            # Display TLE information
-            print("\nTLE Information:")
-            print(f"Name: {self.tle_data[0]}")
-            print(f"TLE Line 1: {self.tle_data[1]}")
-            print(f"TLE Line 2: {self.tle_data[2]}")
-            time.sleep(1.0)
-
-observer_latitude = '23.7719'
-observer_longitude = '90.3892'
-satellite_name = input("Enter Name: ")
-
-tracker = SatelliteTracker(observer_latitude, observer_longitude, satellite_name)
-tracker.track()
+    def get_tle_data(self):
+        """
+        Return the TLE data.
+        """
+        return self.tle_data
